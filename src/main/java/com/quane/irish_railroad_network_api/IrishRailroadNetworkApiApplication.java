e74027be6e95a9b876b909f9e06452525f90bcc0
Jack Quane <jackquane1@gmail.com>
2021-10-20 16:25:45 +0100

    First commit

---
+++ src/main/java/com/quane/irish_railroad_network_api/IrishRailroadNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.quane.irish_railroad_network_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrishRailroadNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrishRailroadNetworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quane/irish_railroad_network_api/IrishRailroadNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quane.irish_railroad_network_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrishRailroadNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
